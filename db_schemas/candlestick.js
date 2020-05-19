6ce166347571ee197526dce998c0b2cc85d3219f
Henry Chen <henrychen385@gmail.com>
2020-05-19 23:43:53 +0800

    uhmmm...

--- db_schemas/candlestick.js
+++ db_schemas/candlestick.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 console.log(process.env.DATABASE)
-mongoose.connect(`${process.env.DATABASE}`, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb://db`, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const Candlestick = new mongoose.model('candlestick', {
     timestamp: Date,
