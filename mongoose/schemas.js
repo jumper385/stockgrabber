810b753e2b1a1934cf33aef6eafc5b0da663b81a
chenry <henrychen385@gmail.com>
2020-05-23 00:34:04 +0800

    switched to use process defined database

--- mongoose/schemas.js
+++ mongoose/schemas.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DATABASE || 'mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const Candlestick = new mongoose.model('candlestick', {
     timestamp: Date,
