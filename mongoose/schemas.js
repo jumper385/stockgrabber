8ff0717f611499fec0b17eadbcef071a88881237
chenry <henrychen385@gmail.com>
2020-05-23 00:43:10 +0800

    logged the database...

--- mongoose/schemas.js
+++ mongoose/schemas.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE || 'mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
-
+console.log(process.env.DATABASE)
 const Candlestick = new mongoose.model('candlestick', {
     timestamp: Date,
     symbol: { type: String, required: true },
